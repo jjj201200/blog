d1ac80d4b27c7891ca38649f754b23ed73d9096e
jjj201200 <jjj201200@gmail.com>
2018-03-21 16:46:09 +0800

    :art:

---
+++ config/plugin.prod.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// add you build-in plugin here, example:
+// exports.nunjucks = {
+//   enable: true,
+//   package: 'egg-view-nunjucks',
+// };
+exports.mongoose = {
+    enable: true,
+    package: 'egg-mongoose',
+};
+
+exports.validate = { // 用于对请求参数进行校验
+    enable: true,
+    package: 'egg-validate',
+};
+
